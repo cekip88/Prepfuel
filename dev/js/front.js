6ca31cd96bea2f1112ac1e7e1ee22ab9bd77f744
cekip88 <cekip@mailr.u>
2022-03-24 14:13:27 +0600

    Init sample

---
+++ dev/js/front.js
@@ -0,0 +1,11 @@
+import { G_Bus } from "./libs/G_Bus.js";
+import { _front } from "./libs/_front.js";
+class Front extends _front{
+  constructor(){
+    super();
+    const _ = this;
+    // G_Bus
+  }
+	
+}
+new Front();
