4eb7b7aeab24961dc82edc96e2517b14c070b0d5
cekip88 <cekip@mailr.u>
2022-03-30 18:12:34 +0600

    student dashboard main Practice schedule Upcoming tutoring sessions

--- dev/js/front.js
+++ dev/js/front.js
@@ -6,6 +6,8 @@ class Front extends _front{
     super();
     const _ = this;
     G_Bus.on('navigate',_.navigate.bind(_))
+      .on('subnavigate',_.subnavigate.bind(_))
+      .on('getSessions',_.getSessions.bind(_))
   }
 
   ascent(item,targetCls,endCls){
@@ -22,6 +24,15 @@ class Front extends _front{
     if (item) item.classList.remove(cls)
   }
 
+
+  // get list of sessions
+  getSessions(clickData){
+    const _ = this;
+
+  }
+
+
+  // methods to navigate blocks from main nav and sub main nav
   navigationInit(list) {
     const _ = this;
     if (!list) return;
@@ -31,6 +42,25 @@ class Front extends _front{
       let activeBtn = list.querySelector('.active');
       if (activeBtn) _.showActiveNavItem(activeBtn,list);
     })
+  }
+  navigate(clickData){
+    const _ = this;
+    let
+      list = clickData.item,
+      target = clickData.event.target,
+      btn = _.ascent(target,'navigate-item','navigate-list');
+
+    _.showActiveNavItem(btn,list);
+    _.changeActiveNavItem(btn);
+  }
+  subnavigate(clickData){
+    const _ = this;
+    if (!clickData) return;
+    let
+      target = clickData.event.target,
+      btn = _.ascent(target,'subnavigate-button','subnavigate');
+
+    _.changeActiveNavItem(btn);
   }
   setActiveNavItem(list){
     const _ = this;
@@ -45,16 +75,6 @@ class Front extends _front{
     } else if (activeBtn){
       _.navigate({item:list, event:{target:activeBtn}})
     }
-  }
-  navigate(clickData){
-    const _ = this;
-    let
-      list = clickData.item,
-      target = clickData.event.target,
-      btn = _.ascent(target,'navigate-item','navigate-list');
-
-    _.showActiveNavItem(btn,list);
-    _.changeActiveNavItem(btn)
   }
   changeActiveNavItem(item){
     const _ = this;
@@ -71,10 +91,26 @@ class Front extends _front{
       label = list.querySelector('.navigate-label');
     label.style = `display:block;width: ${width}px;left: ${x}px;`;
   }
+  changeTab(btn,parentCls){
+    const _ = this;
+    let
+      list = btn.parentElement.children,
+      parent = btn.closest('.' + parentCls),
+      targetSelector = parent.getAttribute('data-tabs'),
+      tabsContainer = _.f(targetSelector);
+
+    if (!targetSelector || !tabsContainer) return;
+
+    _.removeCls(tabsContainer.querySelector(`${targetSelector}>.active`),'active');
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === btn && tabsContainer.children[i]) tabsContainer.children[i].classList.add('active');
+    }
+  }
 
   init(){
     const _ = this;
     _.navigationInit(document.querySelector('.navigate-list'));
+    _.subnavigate({event:{target:_.f('.subnavigate-button.active')}})
   }
 }
 new Front();
