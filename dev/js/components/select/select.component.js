a32f3e7c7d0df8b4ff5290adc54b1c6ec474ce02
Антон Сметанин <foremost186@gmail.com>
2022-06-29 18:22:49 +0600

    - fix custom components

--- dev/js/components/select/select.component.js
+++ dev/js/components/select/select.component.js
@@ -162,6 +162,217 @@ export default class GSelect extends GComponent {
 		
 		_.trigger('appended');
 	}
+	
+	styleSheets() {
+		return `
+			${super.styleSheets()}
+			:host {
+			  --body-display: none;
+			  --body-max-height: 0px;
+			}
+			.g-select {
+			  width: 100%;
+			  height: 100%;
+			  min-width: 80px;
+			  position: relative;
+			  border: 1px solid #dbdbdb;
+			}
+			.g-select-head {
+			  width: 100%;
+			  height: 30px;
+			  cursor: pointer;
+			  display: flex;
+			  align-items: center;
+			  justify-content: space-between;
+			  background-color: #fff;
+			  white-space: normal;
+			  transition: 0.35s ease;
+			  padding: 5px 10px;
+			}
+			.g-select-body {
+			  width: 100%;
+			  max-height: var(--body-max-height);
+			  display: flex;
+			  flex-direction: column;
+			  align-items: flex-start;
+			  background-color: #fff;
+			  overflow: hidden;
+			  position: absolute;
+			  left: 0;
+			  top: 100%;
+			  z-index: 2;
+			  transition: 0.2s ease;
+			}
+			.g-select.active .g-select-body {
+			  overflow: auto;
+			  border: 1px solid #dbdbdb;
+			}
+			.g-select.active .g-select-head:after {
+			  transform: rotate(-45deg);
+			}
+			.g-select-option {
+			  flex: 0 0 30px;
+			  width: 100%;
+			  margin-bottom: 1px;
+			  padding: 0 10px;
+			  text-align: left;
+			  display: flex;
+			  justify-content: flex-start;
+			  align-items: center;
+			}
+			.g-select-option:hover, .g-select-option.active {
+			  background-color: #dbdbdb;
+			}
+			.g-select-arrow {
+			  width: 20px;
+			}
+			.g-select-arrow svg {
+			  width: 10px;
+			  height: 10px;
+			}
+			.g-select-gray {
+			  width: 100%;
+			  height: 42px;
+			  background-color: #F5F8FA;
+			  border: none;
+			  border-radius: 6px;
+			}
+			.g-select-gray .g-select-head {
+			  height: 100%;
+			  padding: 5px 16px;
+			  background-color: initial;
+			  color: #5E6278;
+			}
+			.g-select-gray .g-select-title {
+			  font-family: var(--f_medium);
+			}
+			.g-select-gray .g-select-body {
+			  padding: 0 4px;
+			  background: #FFF;
+			  box-shadow: 0 0 1px rgba(15, 23, 42, 0.06), 0 10px 15px -3px rgba(15, 23, 42, 0.1), 0px 4px 6px -2px rgba(15, 23, 42, 0.05);
+			  border-radius: 8px;
+			}
+			.g-select-gray .g-select-option {
+			  flex: 0 0 32px;
+			  padding: 7px 8px;
+			  margin-bottom: 4px;
+			  display: flex;
+			  align-items: center;
+			  position: relative;
+			  font-family: var(--f_medium);
+			}
+			.g-select-gray .g-select-option:first-child {
+			  border-radius: 4px 4px 0 0;
+			}
+			.g-select-gray .g-select-option:last-child {
+			  border-radius: 0 0 4px 4px;
+			  margin-bottom: 0;
+			}
+			.g-select-gray .g-select-option.hover, .g-select-gray .g-select-option.active {
+			  background-color: #ECF8FF;
+			  color: #00A3FF;
+			}
+			.g-select-gray .g-select-option.active:after {
+			  display: block;
+			  content: "";
+			  width: 16px;
+			  height: 16px;
+			  content: url("img/checkmark-blue.svg");
+			  position: absolute;
+			  right: 10px;
+			}
+			.g-select-gray.active .g-select-body {
+			  padding: 4px;
+			  border: none;
+			}
+			
+			.filter-select, .adding-select {
+			  width: 100%;
+			  height: initial;
+			  border: none;
+			}
+			.filter-select .g-select-head, .adding-select .g-select-head {
+			  height: 34px;
+			  padding: 0 12px;
+			  border-radius: 6px;
+			  background-color: rgb(var(--neutral-100));
+			  color: rgb(var(--neutral-500));
+			  font-family: var(--f_bold);
+			}
+			
+			.adding-select .g-select-head {
+			  height: 44px;
+			}
+			.adding-select .g-select-arrow {
+			  fill: #B5B5C3;
+			}
+			.adding-select .g-select-title {
+			  font: 14px "roboto-regular";
+			}
+			.adding-select.active .g-select-body {
+			  box-shadow: 0px 0px 1px rgba(15, 23, 42, 0.06), 0px 10px 15px -3px rgba(15, 23, 42, 0.1), 0px 4px 6px -2px rgba(15, 23, 42, 0.05);
+			  border-radius: 8px;
+			  border: 0;
+			  top: calc(100% + 4px);
+			}
+			.adding-select .g-select-option {
+			  font: 14px "roboto-medium";
+			  padding: 9px 12px;
+			  color: #5E6278;
+			}
+			
+			.with-arrow:after {
+			  width: 5px;
+			  height: 5px;
+			  content: "";
+			  display: block;
+			  flex: 0 0 5px;
+			  margin-left: 5px;
+			  border-top: 2px solid #000;
+			  border-right: 2px solid #000;
+			  transform: rotate(135deg);
+			  transition: 0.35s ease;
+			}
+			
+			.input-time .g-select-head {
+			  height: 50px;
+			}
+			.input-time .g-select-title {
+			  font-weight: 400;
+			  color: #5E6278;
+			}
+			
+			.head-select {
+			  display: flex;
+			  align-items: center;
+			  justify-content: center;
+			  min-width: initial;
+			  border: none;
+			  color: #fff;
+			  font-size: 12px;
+			}
+			.head-select .g-select-head {
+			  height: 100%;
+			  background-color: rgba(255, 255, 255, 0.08);
+			  border-radius: 8px;
+			}
+			.head-select .with-arrow:after {
+			  border-color: #fff;
+			  transform: rotate(135deg) translate(-2px, 2px);
+			}
+			
+			@media screen and (min-width: 768px) {
+			  .head-select .g-select-head {
+			    padding: 5px 14px;
+			    font-family: var(--f_medium);
+			  }
+			}
+			.table-filter {
+			  font-size: calc(12em/14);
+			}
+		`;
+	}
+	
 	disconnectedCallback() {
 	}
 	static get observedAttributes() {
