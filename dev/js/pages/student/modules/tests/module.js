1cf8ee00eaa2d66cc3894703aca9f75642e3cbc1
Антон Сметанин <foremost186@gmail.com>
2022-07-20 20:24:56 +0600

    - get current parents - get current students

--- dev/js/pages/student/modules/tests/module.js
+++ dev/js/pages/student/modules/tests/module.js
@@ -602,6 +602,7 @@ export class TestsModule extends StudentPage{
 	
 	async getQuestionTpl(){
 		const _ = this;
+		console.log(_._$.currentQuestion['questionType']);
 		return await _[`${_.types[_._$.currentQuestion['questionType']]}Question`]();
 	}
 	flexible(section){
