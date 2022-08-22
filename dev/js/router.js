101735dc8fef38b21315d6a997fddbcee6252850
cekip88 <cekip@mailr.u>
2022-08-22 18:08:04 +0600

    router fix

--- dev/js/router.js
+++ dev/js/router.js
@@ -62,10 +62,12 @@ export class router {
 			pathParts = pathName.split('/').splice(1),
 			module = pathParts.splice(0,1)[0];
 		rawParams[0] = rawParams[0].replace('?','');
-		if(pathParts[0].indexOf('?') > -1){
-			params['module'] = pathParts[0].substr(0,pathParts[0].indexOf('?'));
-		}else{
-			params['module'] = pathParts[0];
+		if(pathParts && pathParts.length){
+			if(pathParts[0].indexOf('?') > -1){
+				params['module'] = pathParts[0].substr(0,pathParts[0].indexOf('?'));
+			}else{
+				params['module'] = pathParts[0];
+			}
 		}
 		for(let i =0; i <pathParts.length; i++){
 			pathParts[i] = pathParts[i].substr(0,pathParts[i].indexOf('?'));
